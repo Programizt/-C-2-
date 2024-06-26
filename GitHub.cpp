8aac968fdae59a3117b3e4325724d0a22ad10a6c
Пётр <int_main_std@mail.ru>
2024-06-26 19:18:34 +0300

    Add files via upload

---
+++ GitHub.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <malloc.h>
+int main()
+{
+	int size;
+	scanf("%d", &size);
+	int* arr = (int*)malloc(sizeof(int) * size);
+	for (int i = 0; i < size; i++) {
+		scanf("%d",&arr[i]);
+	}
+	for (int i = 0; i < size; i++) {
+		printf("arr[");
+		printf("%d", i);
+		printf("] = %d\n", arr[i]);
+	}
+	free(arr);
+}
